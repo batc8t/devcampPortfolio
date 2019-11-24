8051039637e1d833f199e8e8fa0b17344671225b
batc8t <batc8t@gmail.com>
2019-11-24 14:52:21 +0800

    tinkered with some minor styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,8 +34,8 @@ puts "5 skills created"
 		ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
 		in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
 		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		main_image: "https://via.placeholder.com/600x400",
-		thumb_image:"https://via.placeholder.com/350x200"
+		main_image: "https://via.placeholder.com/600x400?text=GHOST + DEVCAMP",
+		thumb_image:"https://via.placeholder.com/350x200?text=GHOST + DEVCAMP"
 	)
 end
 puts "9 portfolios created"
