930b5eb8c3c0130b6d99ca1831b0b798906d08e3
batc8t <batc8t@gmail.com>
2019-11-24 15:27:31 +0800

    intergrated new and create actions for portfolios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
 	def index
 		@portfolio_items = Portfolio.all 
 	end
+
+	def new 
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+	    respond_to do |format|
+		      if @portfolio_item.save
+		        format.html { redirect_to portfolios_path, notice: 'YAHOO! You portfolio item is now live.' }
+		        
+		      else
+		        format.html { render :new }
+		        
+		      end
+	    end
+  end
 end
